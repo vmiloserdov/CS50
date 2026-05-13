201baa4a11128aaaaae66335a7514fee3fb61038
Vlad <vmiloser@andrew.cmu.edu>
2026-05-13 16:18:25 -0400

    Traffic Final accuracy and loss in README.md, final model in model.keras

---
+++ traffic/README.md
@@ -0,0 +1,2 @@
+Final Accuracy: 0.9989 
+Final Loss: 0.0039
---
+++ traffic/traffic.py
@@ -0,0 +1,128 @@
+import cv2
+import numpy as np
+import os
+import sys
+import tensorflow as tf
+
+from sklearn.model_selection import train_test_split
+
+EPOCHS = 20
+IMG_WIDTH = 30
+IMG_HEIGHT = 30
+NUM_CATEGORIES = 43
+TEST_SIZE = 0.2
+
+
+def main():
+
+    # Check command-line arguments
+    if len(sys.argv) not in [2, 3]:
+        sys.exit("Usage: python traffic.py data_directory [model.h5]")
+
+    # Get image arrays and labels for all image files
+    images, labels = load_data(sys.argv[1])
+
+    # Split data into training and testing sets
+    labels = tf.keras.utils.to_categorical(labels)
+    x_train, x_test, y_train, y_test = train_test_split(
+        np.array(images), np.array(labels), test_size=TEST_SIZE
+    )
+
+    # Get a compiled neural network
+    model = get_model()
+
+    # Fit model on training data
+    model.fit(x_train, 
+              y_train, 
+              epochs=EPOCHS, 
+              callbacks=[tf.keras.callbacks.ReduceLROnPlateau(monitor='val_loss', patience=3)], 
+              validation_split=0.2
+            )
+
+    # Evaluate neural network performance
+    model.evaluate(x_test,  y_test, verbose=2)
+
+    # Save model to file
+    if len(sys.argv) == 3:
+        filename = sys.argv[2]
+        model.save(filename)
+        print(f"Model saved to {filename}.")
+
+
+def load_data(data_dir):
+    """
+    Load image data from directory `data_dir`.
+
+    Assume `data_dir` has one directory named after each category, numbered
+    0 through NUM_CATEGORIES - 1. Inside each category directory will be some
+    number of image files.
+
+    Return tuple `(images, labels)`. `images` should be a list of all
+    of the images in the data directory, where each image is formatted as a
+    numpy ndarray with dimensions IMG_WIDTH x IMG_HEIGHT x 3. `labels` should
+    be a list of integer labels, representing the categories for each of the
+    corresponding `images`.
+    """
+    images, labels = [], []
+    for number in os.listdir(data_dir):
+        category_dir = os.path.join(data_dir, number)
+        for image in os.listdir(category_dir):
+            image_dir = os.path.join(category_dir, image)
+
+            bgr = cv2.imread(image_dir)
+            resized = cv2.resize(bgr, (IMG_HEIGHT, IMG_WIDTH))
+
+            images.append(resized)
+            labels.append(int(number))
+
+
+    return (images, labels)
+
+
+def get_model():
+    """
+    Returns a compiled convolutional neural network model. Assume that the
+    `input_shape` of the first layer is `(IMG_WIDTH, IMG_HEIGHT, 3)`.
+    The output layer should have `NUM_CATEGORIES` units, one for each category.
+    """
+
+    model = tf.keras.models.Sequential([
+        tf.keras.layers.Input(shape=(IMG_WIDTH, IMG_HEIGHT, 3)),
+        
+        tf.keras.layers.Conv2D(32, (3, 3), padding="same", activation="relu"),
+        tf.keras.layers.BatchNormalization(),
+        tf.keras.layers.Conv2D(32, (3, 3), padding="same", activation="relu"),
+        tf.keras.layers.BatchNormalization(),
+        tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
+        tf.keras.layers.Dropout(0.2),
+
+        tf.keras.layers.Conv2D(64, (3, 3), padding="same", activation="relu"),
+        tf.keras.layers.BatchNormalization(),
+        tf.keras.layers.Conv2D(64, (3, 3), padding="same", activation="relu"),
+        tf.keras.layers.BatchNormalization(),
+        tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
+        tf.keras.layers.Dropout(0.3),
+
+        tf.keras.layers.Conv2D(128, (3, 3), padding="same", activation="relu"),
+        tf.keras.layers.BatchNormalization(),
+        tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
+        tf.keras.layers.Dropout(0.4),
+
+        tf.keras.layers.Flatten(),
+        tf.keras.layers.Dense(512, activation="relu"),
+        tf.keras.layers.BatchNormalization(),
+        tf.keras.layers.Dropout(0.5),
+        tf.keras.layers.Dense(NUM_CATEGORIES, activation="softmax")
+    ])
+
+    model.compile(
+        optimizer="adam",
+        loss="categorical_crossentropy",
+        metrics=["accuracy"],
+    )
+
+    return model
+
+
+if __name__ == "__main__":
+    main()
